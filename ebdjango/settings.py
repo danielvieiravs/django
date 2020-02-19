68f87bab8e61065fc4ec3573a938290e15e332c3
danielvieiravs <danielvieiravs@live.com.pt>
2020-02-19 22:23:53 +0000

    Setup

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '52npabfu)qcx(k#f4a)-b*yrbmz7%0#tulj#^tvs+p_th^c&ju'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['django-dev.b2tpnj7xc5.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
