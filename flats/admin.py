7051805b3458cd8f47f6a1af99f2a2a9235ebd8d
danielvieiravs <danielvieiravs@live.com.pt>
2020-03-01 16:19:09 +0000

    Put datetime as string

--- flats/admin.py
+++ flats/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from daterange_filter.filter import DateRangeFilter
 
 from import_export.admin import ImportExportModelAdmin
 
@@ -14,10 +13,9 @@ class FlatAdmin(ImportExportModelAdmin):
     list_display = ('available_from', 'licensor', 'property', 'post_code',
                     'area', 'n_b', 'price_one', 'price_two', 'bills',
                     'min_length')
-    list_filter = ('area', ('available_from', DateRangeFilter), 'licensor',
-                    'property', 'post_code', 'price_one', 'price_two',
-                    'n_b')
-    search_fields = ('licensor', 'property', 'area', 'n_b',
+    list_filter = ('area', 'available_from', 'licensor', 'property',
+                    'post_code', 'price_one', 'price_two', 'n_b')
+    search_fields = ('licensor', 'available_from', 'property', 'area', 'n_b',
                      'price_one', 'price_two', 'post_code')
     inlines = [
         PhotoInline,
